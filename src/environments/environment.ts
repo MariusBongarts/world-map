c4eae603ecb741a9d5627735f1fda290723f8b27
Marius Bongarts <mariusbongarts@web.de>
2020-12-29 16:47:22 +0100

    adds firebase config to secret environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { environmentSecret } from './environment.secret';
+
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyCThyp2XTKCTlJXB7Ej27HY_BL1irnyEnM',
-    authDomain: 'webmarker-3dd8c.firebaseapp.com',
-    databaseURL: 'https://webmarker-3dd8c.firebaseio.com',
-    projectId: 'webmarker-3dd8c',
-    storageBucket: 'webmarker-3dd8c.appspot.com',
-    messagingSenderId: '199422612040',
-    appId: '1:199422612040:web:7a65ac697f99023450be7d'
-  }
+  firebaseConfig: environmentSecret.firebaseConfig
 };
 
 /*
